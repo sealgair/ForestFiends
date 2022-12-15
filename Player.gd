a2b683ca0e411b9383381bca34d467a0b70982d9
Chase Caster <sealgair@gmail.com>
2022-12-14 23:11:40 -0600

    jump

--- Player.gd
+++ Player.gd
@@ -1,27 +1,50 @@
-extends Area2D
+extends KinematicBody2D
 
-export var speed = 100
+export (int) var run_speed = 100
+export (int) var jump_speed = -450
+export (int) var gravity = 1200
+
 var screen_size
 signal hit
+
+var velocity = Vector2()
+var jumping = false
+var size = Vector2(16, 16) # todo: dynamic
 
 func _ready():
 	screen_size = get_viewport_rect().size
 
-func _process(delta):
-	var size = Vector2(16, 16) # todo: dynamic
-	var velocity = Vector2.ZERO
+func get_input():
+	velocity.x = 0
 	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
+		velocity.x += run_speed
 		$AnimatedSprite.flip_h = true
 	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
+		velocity.x -= run_speed
+		$AnimatedSprite.flip_h = false
+		
+	var jump = Input.is_action_just_pressed('move_special')
+	if jump and is_on_floor():
+		jumping = true
+		velocity.y = jump_speed
+
+func _physics_process(delta):
+	get_input()
+	velocity.y += gravity * delta
+	if jumping and is_on_floor():
+		jumping = false
+	velocity = move_and_slide(velocity, Vector2(0, -1))
+	
+	position.x = wrapf(position.x, -size.x, screen_size.x)
+	position.y = wrapf(position.y, -size.y, screen_size.y)
+
+func _process(delta):
+	if velocity.x >= 1:
+		$AnimatedSprite.flip_h = true
+	if velocity.x <= -1:
 		$AnimatedSprite.flip_h = false
 	
 	if velocity.length() > 0:
 		$AnimatedSprite.animation = "walk"
 	else:
 		$AnimatedSprite.animation = "idle"
-	
-	position += velocity * speed * delta
-	position.x = wrapf(position.x, -size.x, screen_size.x)
-	position.y = wrapf(position.y, -size.y, screen_size.y)
