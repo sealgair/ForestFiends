2f20ae0b8f1b71faf6287d0e151a6f59f4cf8505
Chase Caster <sealgair@gmail.com>
2022-12-15 17:58:35 -0600

    multiplayer

--- Player.gd
+++ Player.gd
@@ -1,8 +1,11 @@
 extends KinematicBody2D
 
-export (int) var run_speed = 100
-export (int) var jump_speed = -450
-export (int) var gravity = 1200
+export (int) var player = 1
+
+var inputs = {}
+var run_speed = 100
+var jump_speed = -450
+var gravity = 1200
 
 var screen_size
 signal hit
@@ -13,17 +16,22 @@ var size = Vector2(16, 16) # todo: dynamic
 
 func _ready():
 	screen_size = get_viewport_rect().size
+	# button mappings
+	inputs['right'] = "move_right{p}".format({'p': player}) 
+	inputs['left'] = "move_left{p}".format({'p': player}) 
+	inputs['special'] = "move_special{p}".format({'p': player}) 
+	inputs['attack'] = "move_attack{p}".format({'p': player}) 
 
 func get_input():
 	velocity.x = 0
-	if Input.is_action_pressed("move_right"):
+	if Input.is_action_pressed(inputs['right']):
 		velocity.x += run_speed
 		$AnimatedSprite.flip_h = true
-	if Input.is_action_pressed("move_left"):
+	if Input.is_action_pressed(inputs['left']):
 		velocity.x -= run_speed
 		$AnimatedSprite.flip_h = false
 		
-	var jump = Input.is_action_just_pressed('move_special')
+	var jump = Input.is_action_just_pressed(inputs['special'])
 	if jump and is_on_floor():
 		jumping = true
 		velocity.y = jump_speed
