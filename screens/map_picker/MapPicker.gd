c29d5b3a1c3342f07762095c4cef234bedb07c6a
Chase Caster <sealgair@gmail.com>
2023-02-01 21:59:23 -0600

    map picker timeout

--- screens/map_picker/MapPicker.gd
+++ screens/map_picker/MapPicker.gd
@@ -4,6 +4,7 @@ var map_choices = []
 var cursor = Vector2(1,1)
 var input
 var start_players = []
+var chosing_player = null
 
 func _ready():
 	map_choices = [
@@ -12,14 +13,15 @@ func _ready():
 		[$MapChoice7, $MapChoice8, $MapChoice9],
 	]
 	
-	# randomly pick a (non-computer) player to pick the level
-	var humans = [1]
-	for p in range(start_players.size()):
-		if not start_players[p]['computer']:
-			humans.append(p)
-	var i = Global.rand_choice(humans)
-	input = inputs[0]
-	$PlayerChoice.text = "Player %d chooses" % (i+1)
+	if chosing_player == null:
+		# randomly pick a (non-computer) player to pick the level
+		var humans = [0]
+		for p in range(start_players.size()):
+			if not start_players[p].computer:
+				humans.append(p)
+		chosing_player = Global.rand_choice(humans)
+	input = inputs[chosing_player]
+	$PlayerChoice.text = "Player %d chooses" % (chosing_player+1)
 
 
 func _process(delta):
@@ -36,10 +38,16 @@ func _process(delta):
 		cursor.y = wrapi(cursor.y, 0, 3)
 		found = map_choices[cursor.x][cursor.y].visible
 	map_choices[cursor.x][cursor.y].set_selected(true)
+	if dir.length() > 0:
+		# timer starts after last input
+		$ContinueTimer.start()
 	
 	if input.is_just_pressed("select"):
 		start_game()
-
+	
+	$Countdown.text = ""
+	if $ContinueTimer.time_left <= 5 and not $ContinueTimer.paused:
+		$Countdown.text = String(ceil($ContinueTimer.time_left))
 
 func start_game():
 	var chosen = map_choices[cursor.x][cursor.y]
@@ -47,3 +55,6 @@ func start_game():
 		"start_data": start_players,
 		"map_scene": chosen.map_scene
 	})
+
+func _on_ContinueTimer_timeout():
+	start_game()
