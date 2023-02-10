7c84500558c1417588cd70c8899d2bedc384c6dc
Chase Caster <sealgair@gmail.com>
2023-02-09 20:51:26 -0600

    fix wasp pathing so it doesn't try to go through diagonal gaps it can't

--- brain/WaspPath.gd
+++ brain/WaspPath.gd
@@ -10,5 +10,11 @@ func connect_node(point):
 			if x != 0 or y != 0:
 				var side_pos = pos + Vector2(x, y)
 				var side = get_point_exact(side_pos)
+				if x != 0 and y != 0:
+					# diagonal requires an adjacent side free too
+					var adjacent_x = side_pos - Vector2(x, 0)
+					var adjacent_y = side_pos - Vector2(0, y)
+					if not get_point_exact(adjacent_x) and not get_point_exact(adjacent_x):
+						continue # both sides are unavailable, so we can't go on the diagonal
 				if side:
 					connect_points(point, side)
